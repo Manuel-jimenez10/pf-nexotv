5cf5ab9e3d443e24ac9d3b71c8917057fe527457
kelvinvida99 <kelvinvida99@gmail.com>
2024-08-20 16:30:35 -0400

    update dependencies and app.mondule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // debug: false,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
