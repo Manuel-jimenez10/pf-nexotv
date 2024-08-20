fc5034d5668223634a912d2289e86916d83c1495
kelvinvida99 <kelvinvida99@gmail.com>
2024-08-20 16:42:10 -0400

    users module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,18 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      // debug: false,
       playground: false,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
