ff936caa4d93d6d9e0a635d6de188e3b0acf80b0
kelvinvida99 <kelvinvida99@gmail.com>
2024-08-20 22:59:02 -0400

    Create entity

--- README.md
+++ README.md
@@ -9,21 +9,25 @@
 2. Copiar y renombrar el archivo **.env.template** a **.env**
 
 3. Ejecutar
+
 ```
 yarn install
 ```
 
 4. Levantar la imagen de Docker
+
 ```
 docker-compose up -d
 ```
 
 5. Levantar el servidor
+
 ```
 yarn start:dev
 ```
 
 6. Visitar el sitio
+
 ```
 localhost:3000/graphql
 ```
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,38 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({ name: 'users' })
 @ObjectType()
 export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
+  id: string;
+
+  @Column('text', { unique: true, nullable: false })
+  @Field(() => String)
+  email: string;
+
+  @Column('text', { nullable: false })
+  @Field(() => String)
+  password: string;
+
+  @Column('text', { nullable: false })
+  @Field(() => String)
+  fullName: string;
+
+  @Column('text', { array: true, default: ['basic'] })
+  @Field(() => [String])
+  subscription: string[];
+
+  @Column('text', { array: true })
+  @Field(() => [String])
+  views?: string[];
+
+  @Column('text', { array: true })
+  @Field(() => [String])
+  favorites?: string[];
+
+  @Column('text', { array: true, default: ['user'] })
+  @Field(() => [String])
+  rol: string[];
 }
